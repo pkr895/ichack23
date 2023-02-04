05efc301228688a174e643b942cfee29a495c93f
Kevin Roman <44275689+Kevin-Roman@users.noreply.github.com>
2023-02-04 19:46:11 +0000

    Remove previous styling

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,9 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
-import styles from '@/styles/Home.module.css';
-
-const inter = Inter({ subsets: ['latin'] });
+import Searchbar from '@/components/Searchbar';
 
 export default function Home() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <div className='bg-amber-100'>
+      <Searchbar />
+    </div>
+  );
 }
