4f2f721d99005dcce83a9e47f099ae15c4d0767d
Abdullah Ijaz <abdullahijaz04@outlook.com>
2023-02-05 01:41:43 +0000

    epic post request

---
+++ frontend/.next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST={"testing.js -> @/components/SimpleMap":{"id":"testing.js -> @/components/SimpleMap","files":["static/chunks/components_SimpleMap_js.js"]}}
--- server.py
+++ server.py
@@ -1,11 +1,13 @@
-from flask import Flask, request, render_template, url_for, redirect
+from flask import Flask, jsonify, request, render_template, url_for, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/home")
-def home():
-    return render_template("home.html")
+@app.route("/", methods=['POST'])
+def parseSearch():
+    data = request.get_json()
+    print(data)
+    return jsonify({"lat": 30, "lng": 27})
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="3000")
